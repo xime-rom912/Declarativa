22a4f82b6f08628056b6522b9bd5b4e137a3d7dd
Ximena Romero <a329898@uach.mx>
2021-05-12 17:26:12 -0600

    Modifique actividad 17

--- Actividad17/script.js
+++ Actividad17/script.js
@@ -2,13 +2,13 @@ const app = Vue.createApp({
     data(){
         return{
             estatus: true,
-            estatus2: true,
+            estatus2: false,
             imagen:"images/focoOff.png"
         }
     },
     methods:{
         cambiarEstatus(){
-            if(this.estatus === true){
+            if(this.estatus2 === true){
                 this.imagen = "images/focoOff.png"
                 this.estatus2 = false
             }else {
